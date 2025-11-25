40f6ff9d5d4ea4d64292a61a4ca1bafc0dfcfefe
Jeff Schomay <1825491+jschomay@users.noreply.github.com>
2025-11-25 09:14:47 -0500

    basic grid rendering and navigating

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal, onCleanup, onMount, Show } from 'solid-js'
+import { createEffect } from 'solid-js'
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [showFull, setShowFull] = createSignal(false)
+  const [offset, setOffset] = createSignal({ x: 0, y: 0 })
+
+  const puzzle = [
+    ["t", "h", "i", "s"],
+    ["h", "i", "#", "o"],
+    ["a", "g", "e", "#"],
+    ["t", "h", "#", "#"],
+  ]
+
+  const toggleZoom = () => setShowFull(!showFull())
+  const move = (x: number, y: number) => setOffset(offset => ({
+    x: Math.min(puzzle[0].length - 1, Math.max(0, offset.x + x)),
+    y: Math.min(puzzle.length - 1, Math.max(0, offset.y + y))
+  }))
+
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.code === "Space") {
+      toggleZoom()
+      return
+    }
+
+    if (showFull()) {
+      return
+    } else if (e.code === "ArrowUp") {
+      move(0, -1)
+    } else if (e.code === "ArrowDown") {
+      move(0, 1)
+    } else if (e.code === "ArrowLeft") {
+      move(-1, 0)
+    } else if (e.code === "ArrowRight") {
+      move(1, 0)
+    }
+  }
+
+  onMount(() => document.addEventListener("keydown", handleKeyDown))
+  onCleanup(() => document.removeEventListener("keydown", handleKeyDown))
 
   return (
     <>
-      <div class="flex flex-row justify-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
+      <div class="h-screen w-screen overflow-hidden flex justify-center items-center">
+        <div class="w-lg aspect-square overflow-hidden p-8">
+          <Puzzle offset={offset()} showFull={showFull()} puzzle={puzzle} />
+        </div >
       </div >
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
     </>
   )
+}
+
+function Puzzle(props: { offset: { x: number, y: number }, showFull: boolean, puzzle: string[][] }) {
+  let w = () => props.puzzle[0].length
+  let h = () => props.puzzle.length
+
+
+  let styles = () => ({
+    width: `calc(100% * ${w()})`,
+    height: `calc(100% * ${h()})`,
+    transform: props.showFull ? `scale(${1 / Math.max(w(), h())})` : `translate(-${100 * props.offset.x / w()}%, -${100 * props.offset.y / h()}%)`,
+  })
+
+  return <div
+    style={styles()}
+    class="text-violet-800 grid grid-cols-4 gap-8 transition-transform duration-300 origin-top-left"
+  >
+    {props.puzzle.map((row, r) => row.map((cell, c) => {
+      return <Cell r={r} c={c} value={cell} />
+    }))}
+  </div>
+}
+
+function Cell(props: { r: number, c: number, value: string }) {
+  return <div class={`cell  border-gray-300 border-2 rounded-3xl flex items-center justify-center ${props.value === "#" && "bg-gray-300"}`}>
+    <Show when={props.value !== "#"}>
+      <span class="text-gray-500 uppercase text-9xl">{props.value}</span>
+    </Show>
+  </div>
 }
 
 export default App
