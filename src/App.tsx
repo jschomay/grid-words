ddd9bee2869c34a2b38e24d08f9d8a68b4aca651
Jeff Schomay <1825491+jschomay@users.noreply.github.com>
2025-12-08 19:07:11 -0500

    Complete crosswordle logic

--- src/App.tsx
+++ src/App.tsx
@@ -14,70 +14,89 @@ function makePuzzle(cells: string[][]): Puzzle {
   })
 }
 
-function toCoord({ x, y }: Coord): string {
+function coordToString({ x, y }: Coord): string {
   return `${x},${y}`
 }
 
 const status = (guesses: Record<string, string>, coord: Coord, puzzle: Puzzle) => {
-  const guess = guesses[toCoord(coord)]
+  const guess = guesses[coordToString(coord)]
   if (puzzle.valueAt(coord) === "#") {
-    return "bg-gray-400"
+    return "bg-gray-700"
   } else if (guess && guess.toLowerCase() === puzzle.valueAt(coord).toLowerCase()) {
     return "bg-green-400"
   } else if (rightLetterWrongSpot(guesses, coord, puzzle)) {
     return "bg-yellow-400"
+  } else if (guess) {
+    return "bg-gray-400"
   } else {
-    return ""
+    return "bg-white"
   }
 }
 
-const wordToPuzzleCoords = (puzzle: Puzzle, word: string[], wordIndex: number, direction: "down" | "across"): string[] => {
+const wordToPuzzleCoords = (coord: Coord, word: string[], wordIndex: number, direction: "down" | "across"): string[] => {
   if (direction === "across") {
-    return word.map((_w, i) => toCoord({ x: puzzle.x - wordIndex + i, y: puzzle.y }))
+    return word.map((_w, i) => coordToString({ x: coord.x - wordIndex + i, y: coord.y }))
   } else {
-    return word.map((_w, i) => toCoord({ x: puzzle.x, y: puzzle.y - wordIndex + i }))
+    return word.map((_w, i) => coordToString({ x: coord.x, y: coord.y - wordIndex + i }))
   }
 }
 
 const rightLetterWrongSpot = (guesses: Record<string, string>, coord: Coord, puzzle: Puzzle): boolean => {
-  // get words from coord
-  // get puzzle coords for words
-  // get guessees for words' puzzle coords
-  // remove correct guesses from words
-  // for each word, for each guess up to index of coord in word, if present in word, remove from word
-  // if guess in either words' remaining letters, return true, else false
   let [across, down] = puzzle.wordsAt(coord)
-  const puzzleCoordsDown = wordToPuzzleCoords(puzzle, across[0], across[1], "across")
-  const puzzleCoordsAcross = wordToPuzzleCoords(puzzle, down[0], down[1], "down")
+  across[0] = across[0].map(c => c.toLowerCase())
+  down[0] = down[0].map(c => c.toLowerCase())
+  const puzzleCoordsDown = wordToPuzzleCoords(coord, across[0], across[1], "across")
+  const puzzleCoordsAcross = wordToPuzzleCoords(coord, down[0], down[1], "down")
   const guessesAcross = puzzleCoordsDown.map((coord) => guesses[coord])
   const guessesDown = puzzleCoordsAcross.map((coord) => guesses[coord])
-  across[0] = across[0].filter((v, i) => v.toLocaleLowerCase() !== guessesAcross[i]?.toLowerCase())
-  down[0] = down[0].filter((v, i) => v.toLocaleLowerCase() !== guessesDown[i]?.toLowerCase())
-  // TODO coord bug? flashes whwn puzzle.x/y moves, but logic seems right?
-  // TODO still: already has right letter wrong spot
 
-  return across[0].concat(down[0]).includes(guesses[toCoord(coord)])
+  const redacted = "$"
+
+  // remove correct guess from words
+  across[0] = across[0].map((v, i) => v === guessesAcross[i] ? redacted : v)
+  down[0] = down[0].map((v, i) => v === guessesDown[i] ? redacted : v)
+
+  // remove already guessed right-letter-wrong-spot from words
+  for (let i = 0; i < across[1]; i++) {
+    if (across[0][i] === redacted) continue
+
+    let foundIndex = across[0].indexOf(guessesAcross[i])
+    if (foundIndex >= 0) {
+      across[0][foundIndex] = redacted
+    }
+  }
+  for (let i = 0; i < down[1]; i++) {
+    if (down[0][i] === redacted) continue
+
+    let foundIndex = down[0].indexOf(guessesDown[i])
+    if (foundIndex >= 0) {
+      down[0][foundIndex] = redacted
+    }
+  }
+
+  return across[0].concat(down[0]).includes(guesses[coordToString(coord)])
 }
 
 function App() {
   const cells = [
     ["t", "h", "i", "s"],
     ["h", "i", "#", "o"],
-    ["a", "t", "e", "#"],
+    ["a", "p", "e", "#"],
     ["t", "#", "h", "i"],
   ]
+  const puzzle = makePuzzle(cells)
   const [showFull, setShowFull] = createSignal(true)
-  const [puzzle, setPuzzle] = createStore(makePuzzle(cells))
+  const [coords, setCoords] = createSignal<Coord>({ x: 0, y: 0 })
   const [guesses, setGuesses] = createSignal({})
 
   const toggleZoom = () => setShowFull(!showFull())
   const move = (dx: number, dy: number) => {
-    setPuzzle(produce((p) => p.set({
-      x: Math.min(puzzle.ipuz.dimensions.width - 1, Math.max(0, puzzle.x + dx)),
-      y: Math.min(puzzle.ipuz.dimensions.height - 1, Math.max(0, puzzle.y + dy))
-    })))
+    setCoords(coords => ({
+      x: Math.min(puzzle.ipuz.dimensions.width - 1, Math.max(0, coords.x + dx)),
+      y: Math.min(puzzle.ipuz.dimensions.height - 1, Math.max(0, coords.y + dy))
+    }))
   }
-  const guess = (guess: string) => setGuesses((g) => ({ ...g, [toCoord(puzzle)]: guess }))
+  const guess = (guess: string) => setGuesses((g) => ({ ...g, [coordToString(coords())]: guess }))
 
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.code === "Space") {
@@ -114,7 +133,7 @@ function App() {
             <Word direction="across" word={puzzle.getWord("across")} />
           </div>
           <div class="row-start-2 col-start-2 w-lg aspect-square overflow-hidden p-8">
-            <PuzzleGrid puzzle={puzzle} showFull={showFull()} guesses={guesses()} />
+            <PuzzleGrid coords={coords()} puzzle={puzzle} showFull={showFull()} guesses={guesses()} />
           </div >
           <div class={`col-start-3 row-start-2 justify-self-end self-center transition-opacity ${showFull() && "opacity-0"}`}>
             <Word direction="down" word={puzzle.getWord("down")} />
@@ -130,14 +149,14 @@ function Word(props: { direction: "down" | "across", word: [string[], number] })
   return <div class={`text-3xl uppercase flex ${props.direction === "down" ? "flex-col" : "flex-row"}`}>
     {
       props.word[0].map((w, i) =>
-        <div class={`w-20 aspect-square border-2 border-gray-400 flex items-center justify-center ${colors(i)}`}>
+        <div class={`w-20 aspect-square border-2 border-gray-700 flex items-center justify-center ${colors(i)}`}>
           <span>{w}</span>
         </div>)
     }
   </div>
 }
 
-function PuzzleGrid(props: { puzzle: Puzzle, showFull: boolean, guesses: Record<string, string> }) {
+function PuzzleGrid(props: { coords: Coord, puzzle: Puzzle, showFull: boolean, guesses: Record<string, string> }) {
   let w = props.puzzle.ipuz.dimensions.width
   let h = props.puzzle.ipuz.dimensions.height
 
@@ -146,32 +165,32 @@ function PuzzleGrid(props: { puzzle: Puzzle, showFull: boolean, guesses: Record<
     height: `calc(100% * ${h})`,
     transform: props.showFull
       ? `scale(${1 / Math.max(w, h)})`
-      : `translate(-${100 * props.puzzle.x / w}%, -${100 * props.puzzle.y / h}%)`,
+      : `translate(-${100 * props.coords.x / w}%, -${100 * props.coords.y / h}%)`,
   })
 
   let targetStyles = () => ({
     width: `calc(100% / ${w})`,
-    transform: `translate(${100 * props.puzzle.x}%, ${100 * props.puzzle.y}%)`
+    transform: `translate(${100 * props.coords.x}%, ${100 * props.coords.y}%)`
   })
 
   return <div
     style={styles()}
     class="text-violet-800 grid grid-cols-4 grid-rows-4 gap-8 transition-transform duration-300 origin-top-left"
   >
-    <div style={targetStyles()} class="aspect-square absolute rounded-3xl border-18 border-red-400 transition-transform">0</div>
+    <div style={targetStyles()} class="aspect-square absolute rounded-3xl border-18 border-red-400 transition-transform"></div>
     {props.puzzle.ipuz.solution.map((row, y) => row.map((cell, x) => {
       return <Cell y={y} x={x}
         value={props.puzzle.valueAt({ x, y })}
         status={status(props.guesses, { x, y }, props.puzzle)}
-        guess={props.guesses[toCoord({ x, y })]} />
+        guess={props.guesses[coordToString({ x, y })]} />
     }))}
   </div>
 }
 
 function Cell(props: { y: number, x: number, value: string, status: string, guess: string }) {
-  return <div class={`border-gray-400 border-2 rounded-3xl flex items-center justify-center ${props.status}`}>
+  return <div class={`border-gray-700 border-2 rounded-3xl flex items-center justify-center ${props.status}`}>
     <Show when={props.value !== "#"}>
-      <span class="text-black dark:text-white uppercase text-9xl">{props.guess}/{props.value}</span>
+      <span class="text-black dark:text-white uppercase text-9xl">{props.guess}</span>
     </Show>
   </div>
 }
